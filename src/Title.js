f43b57d329220b3cd56a50572e89e33155f7b4db
Douglas254 <dobara@kabarak.ac.ke>
2021-10-14 15:18:28 +0300

    smart-fridge app

---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div>
+      <h1 style={{ color: "purple", fontSize: "38px" }}>VIRTUAL FRIDGE</h1>
+    </div>
+  );
+};
+
+export default Title;
